4b3809c634b64934ec91996f4608c8ca3e8fb339
qiaoyu <rainoy.me@gmail.com>
2020-02-26 11:13:56 +0800

    组件优化

--- src/pages/bytomgrant.js
+++ src/pages/bytomgrant.js
@@ -5,6 +5,10 @@ import css from 'styled-components';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import Faq from '../components/grant/Faq';
+import Steps from '../components/grant/Step';
+import BountyList from '../components/grant/BountyList';
+
 import _conf from '../conf/bounty.conf';
 
 import img_banner_cn from '../images/bounty/banner-cn.png';
@@ -76,70 +80,14 @@ const Vision = css.div`
     margin: 30px auto;
     line-height: 1.7em;
   }
-  &>ul{
-    margin-top: 70px;
-    display: flex;
-    justify-content: space-between;
-  }
   @media (max-width: 640px) {
     width: 100%;
     padding: 0 16px;
     &>h1{ font-size: 36px; }
     &>p{ font-size: 16px; width: 100%; }
-    &>ul{
-      flex-direction: column;
-      margin-top: 40px;
-    }
   }
 `;
-const StepItem = css.li`
-  display: inline-block;
-  width: 222px;
-  height: 158px;
-  position: relative;
-  &>div {
-    width: 192px;
-    height: 158px;
-    padding: 20px 20px 20px 50px;
-    text-align: left;
-    background: #fff;
-    position: absolute;
-    right: 0;
-    top: 0;
-    h1{
-      font-size: 18px;
-      color: rgba(0, 0, 0, 0.88);
-    }
-    p{
-      font-size: 16px;
-      color: rgba(0, 0, 0, 0.56);
-      line-height: 1.8em;
-      margin-top: 10px;
-    }
-  }
-  &>img{
-    width: 60px;
-    height: 60px;
-    position: absolute;
-    left: 0;
-    ${props => props.pos ? 'top: 0;' : 'bottom: 0;'}
-  }
-  @media (max-width: 640px) {
-    width: 100%;
-    height: 100px;
-    margin-top: 20px;
-    &>div{
-      width: auto;
-      height: 100px;
-      left: 30px;
-      h1{ font-size: 16px; }
-      p{ font-size: 14px; }
-    }
-    &>img{
-      top: 0 !important;
-    }
-  }
-`;
+
 const Button = css.a`
   display: inline-block;
   padding: 0 32px;
@@ -163,89 +111,6 @@ const Button = css.a`
     font-size: 14px;
   }
 `;
-const BountyListWrap = css.div`
-  background: #fff;
-`;
-const BountyList = css.div`
-  width: 1240px;
-  margin: 0 auto;
-  padding: 100px 0;
-  display: flex;
-  justify-content: space-between;
-  &>h1 {
-    font-size: 44px;
-    font-weight: bold;
-    line-height: 1.5em;
-    width: 190px;
-  }
-  &>section{
-    flex: 1;
-    padding-left: 126px;
-    &>div {
-      clear: both;
-      &:not(:first-child) {
-        padding-top: 80px;
-      }
-      &>h1{
-        font-size: 28px;
-        line-height: 1.6em;
-        color: rgba(0, 0, 0, 0.88);
-        border-bottom: 1px solid #d8d8d8;
-        padding-bottom: 20px;
-      }
-      &>ul {
-        clear: both;
-        li{
-          width: 280px;
-          min-height: 175px;
-          margin: 32px 0 0 32px;
-          float: left;
-          background: #fafafa;
-          padding: 20px;
-          h3{
-            font-size: 18px;
-            line-height: 1.5em;
-          }
-          p{
-            font-size: 16px;
-            color: rgba(0, 0, 0, 0.56);
-            line-height: 1.8em;
-          }
-          p:first-of-type{
-            margin-top: 20px;
-          }
-          &:hover{
-            h3{ color: #004EE4; }
-          }
-        }
-        li:nth-child(1), li:nth-child(4), li:nth-child(7) {
-          margin-left: 0;
-        }
-      }
-    }
-  }
-  @media (max-width: 640px) {
-    width: 100%;
-    padding: 40px 20px;
-    flex-direction: column;
-    &>h1{ font-size: 36px; margin-bottom: 40px; }
-    &>section {
-      padding: 0;
-      &>div {
-        clear: both;
-        ul{
-          li { 
-            width: 100%; 
-            min-height: 125px;
-            margin: 20px 0 0 0; 
-            h3{ font-size: 16px; }
-            p{ font-size: 14px; }
-          }
-        }
-      }
-    }
-  }
-`;
 const SubBannerWrap = css.div`
   width: 100%;
   height: 280px;
@@ -266,39 +131,12 @@ const SubBannerWrap = css.div`
     line-height: 1.6em;
     text-align: center;
   }
-`;
-const FaqWrap = css.div`
-  background: #1C1C1C;
-  color: #fff;
-  padding: 100px 0;
-`;
-const Faq = css.div`
-  width: 1240px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  &>h1{
-    color: #fff;
-    font-size: 44px;
-  }
-  &>div{
-    padding-left: 238px;
-    &>div{
-      border-bottom: 1px solid #363636;
-      padding: 40px 0;
-      &:first-child{
-        padding-top: 0;
-      }
-      &>h2{
-        font-size: 18xp;
-        color: #fff;
-      }
-      &>p{
-        font-size: 16px;
-        margin-top: 10px;
-        line-height: 1.8em;
-        color: rgba(255, 255, 255, 0.72);
-      }
+  @media (max-width: 640px) {
+    width: 100%;
+    height: 230px;
+    background: url(${img_m_sub_banner}) top center / 100% 250px no-repeat;
+    &>img{
+      display: none;
     }
   }
 `;
@@ -309,9 +147,7 @@ function Bounty({ intl }) {
     <Layout light disableSubnav>
       <SEO title={{zh: '赏金计划', en: 'Bytom Grant'}} />
       <Banner>
-        <BannerCont lang={lang} style={{position: 'relative'}}>
-          
-        </BannerCont>
+        <BannerCont lang={lang} style={{position: 'relative'}} />
         <BannerFront />
       </Banner>
       <ContWrap>
@@ -319,70 +155,15 @@ function Bounty({ intl }) {
           <Msg id="bounty_vision_title" tagName="h1" />
           <Msg id="bounty_vision_des" tagName="p" />
           <Button href="">Apply Now</Button>
-          <ul>
-            {
-              _conf.steps.map((item, index) => (
-                <StepItem pos={index % 2 === 1} key={index}>
-                  <div>
-                    <h1>{item.title}</h1>
-                    <p>{item.cont}</p>
-                  </div>
-                  <img src={item.icon} alt=""/>
-                </StepItem>
-              ))
-            }
-          </ul>
+          <Steps />
         </Vision>
       </ContWrap>
-      <BountyListWrap>
-        <BountyList>
-          <Msg id="bounty_list_title" tagName="h1" />
-          <section>
-            {
-              _conf.task_list.map((item, index) => (
-                <div key={index}>
-                  <h1>{item.title}</h1>
-                  <ul>
-                    {
-                      item.list.map((task, taskIndex) => (
-                        <li key={taskIndex}>
-                          <h3>0{taskIndex + 1}</h3>
-                          <p>{task.title}</p>
-                          {
-                            task.children && task.children.map((taskItem, taskItemIndex) => (
-                              <p key={taskItemIndex}>{taskItem}</p>
-                            ))
-                          }
-                        </li>
-                      ))
-                    }
-                  </ul>
-                </div>
-              ))
-            }
-          </section>
-        </BountyList>
-      </BountyListWrap>
+      <BountyList />
       <SubBannerWrap>
         <img src={img_coins} alt=""/>
         <Msg id="bounty_banner_subtitle" tagName="h1" />
       </SubBannerWrap>
-      <FaqWrap>
-        <Faq>
-          <Msg id="bounty_faq_title" tagName="h1"/>
-          <div>
-            {
-              _conf.faq.map((item, index) => (
-                <div key={index}>
-                  <h2>{item.title}</h2>
-                  <p>{item.cont}</p>
-                  { index === 0 && <Button style={{ marginTop: 15 }}><Msg id="bounty_btn_apply" /></Button>}
-                </div>
-              ))
-            }
-          </div>
-        </Faq>
-      </FaqWrap>
+      <Faq />
     </Layout>
   )
 };
