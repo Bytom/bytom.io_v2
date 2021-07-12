8dc4972192ea2e5857e9358426d91a88efc3a2e0
qiaoyu <rainoy.me@gmail.com>
2021-07-12 23:31:56 +0800

    feat: update api host

--- src/conf/config.js
+++ src/conf/config.js
@@ -3,7 +3,7 @@ import { Link, injectIntl, FormattedMessage as Msg } from "gatsby-plugin-intl";
 
 export default {
   api: {
-    news: 'https://webapi.8btc.com/bytom/news/list', //page_sie, page, lang = en,zh
+    news: 'https://webapi.8btc.cn/bytom/news/list', //page_sie, page, lang = en,zh
     faucet: 'https://kit.blockmeta.com/api/v1/get_testnet_coins',
     eventsList: 'https://www.yuque.com/api/v2/repos/biyuanlian/gguo3r/docs',
     eventsDetail: 'https://www.yuque.com/api/v2/repos/biyuanlian/gguo3r/docs/',
